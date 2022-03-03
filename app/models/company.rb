35b2136f45eaa0e1e101f141f671d037f72a2847
Daniel Zhang <dzhangwh@gmail.com>
2022-03-03 04:49:29 -0500

    done

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,11 +3,20 @@ class Company < ActiveRecord::Base
     has_many :devs, through: :freebies
 
     def give_freebie (dev, item_name, value)
-        Freebie.create({       
+        Freebie.create({      
             item_name: item_name,
             value: value,
             dev_id: dev.id,
-            company_id:
+            company_id: self.id
         })
     end
+
+    def self.oldest_company
+        self.all.min_by  { |c| c.founding_year}
+        #self.order(:founding_year).first
+    end
 end
+
+
+
+# Company.first.give_freebie(Dev.all.sample, "beans", 20)
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,23 @@
 class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        self.freebies.find_by({item_name:  item_name}) ? true : false
+    end
+
+    def give_away(friend_dev, freebie)
+        if self.received_one?(freebie.item_name)
+            freebie.update({dev_id: friend_dev.id})
+        
+        else
+            "not yours"
+        end
+    end
 end
+
+#dev1 = Dev.all.sample
+#dev2 = Dev.last
+#freeb1 = Freebie.all.sample
+
+#dev1.give_away(dev2, freeb1)
